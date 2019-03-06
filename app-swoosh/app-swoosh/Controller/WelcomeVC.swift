c37e936e0ff317fb4c616ab08e484f93ab70c14a
Equalocalize <equalocalize@MacBook-Pro-de-Equalocalize.local>
2019-03-05 19:17:43 -0500

    Lesson-07 debugging

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+        
     @IBAction func unwindFromSkillVC(UnwindSegue: UIStoryboardSegue) {
     
     }
 
 
 }
-
